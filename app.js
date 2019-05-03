e79d7df3dc639b5fbaf685739003d6e2e2e0cbc4
Nick Weber <webernick@icloud.com>
2019-05-03 12:19:05 -0700

    Adds pug and templates

--- app.js
+++ app.js
@@ -3,16 +3,25 @@
 // ------ MODULES -----------
 const express = require('express');
 
-// ---- GLOBAL CONSTANTS -----
-
-
 // -------- SETUP EXPRESS -----
+// new express app
 const app = express();
-// local server for development
-app.listen(3000)
 
-// -- ROUTES --
-app.get('/', (request, response) => {
-  response.send('I love Jam');
+// tell express to use 'pug' templating
+app.set('view engine', 'pug');
+
+// local server for development
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000');
 });
 
+// -- HELPERS --
+
+// -- ROUTES --
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/cards', (req, res) => {
+  res.render('card', {prompt: "Who is buried in Grant's tomb"});
+});
